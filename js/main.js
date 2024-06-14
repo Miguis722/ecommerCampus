9b96f16c1d039be60076678c01dbc55c80b32dfe
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-06-14 08:12:48 -0500

    fix: :ambulance: Menú inicio
    Solucionado el menú de inicio para que funcione y muestre la data de la API

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 import { menuListCategoryIndex } from "./components/menu.js";
 import { galleryIndex } from "./components/gallery.js";
-import { getAllproductName, getAllCategory } from "./module/app.js";
+import { getAllProductName, getAllCategory } from "./module/app.js";
 
 
 let input__search = document.querySelector("#input__search");
 let main__article = document.querySelector(".main__article");
 let nav__ul = document.querySelector(".nav__ul");
 
-addEventListener("DOMContentLoaded", async e => {
+addEventListener("DOMContentLoaded", async e=>{
     if(!localStorage.getItem("getAllCategory")) localStorage.setItem("getAllCategory", JSON.stringify(await getAllCategory()));
-    nav__ul.innerHTML = await menuListCategoryIndex(JSON.parse(localStorage.getItem("getAllCategory")));
+    nav__ul.innerHTML = await menuListCategoryIndex(JSON.parse(localStorage.getItem("getAllCategory")));  
 })
 
 
@@ -17,6 +17,6 @@ input__search.addEventListener("change", async e => {
     let params = new URLSearchParams(location.search);
     let data = { search : e.target.value, id: params.get('id')}
     input__search.value = null;
-    let res = await getAllproductName(data)
+    let res = await getAllProductName(data)
     main__article.innerHTML = galleryIndex(res, params.get('id'));
 });
--- js/module/app.js
+++ js/module/app.js
@@ -1,28 +1,26 @@
 import { headers } from "../components/env.js";
 
-export const getAllproductName = async({search:text} = {search:"Phone"}) => {
+export const getAllProductName = async({search:text, id:idCategory})=>{
     console.log("Esperando ...");
-    const url = `https://real-time-amazon-data.p.rapidapi.com/search?query=${text}&page=1&country=US&sort_by=RELEVANCE&product_condition=ALL`;
-    
-const options = {
-	method: 'GET',
-	headers
-};
-let res = await fetch(url, options);
-let data = res.json();
-return data;
+    console.log(text, idCategory);
+    const url = `https://real-time-amazon-data.p.rapidapi.com/search?query=${text}&page=1&country=US&sort_by=RELEVANCE&category_id=${idCategory}&product_condition=ALL`;
+    const options = {
+        method: 'GET',
+        headers
+    };
+    let res = await fetch(url, options);
+    let data = res.json();
+    return data;
 }
 
-export const getAllCategory = async() => {
-    console.log("Esperando ...")
-    const url = 'https://real-time-amazon-data.p.rapidapi.com/products-by-category?category_id=2478868012&page=1&country=US&sort_by=RELEVANCE&product_condition=ALL';
-
-const options = {
-	method: 'GET',
-	headers
-};
-let res=await fetch(url, options);
-let data = res.json();
-return data;
+export const getAllCategory = async()=>{
+    console.log("Esperando ...");
+    const url = `https://real-time-amazon-data.p.rapidapi.com/product-category-list?country=US`;
+    const options = {
+        method: 'GET',
+        headers
+    };
+    let res = await fetch(url, options);
+    let data = res.json();
+    return data;
 }
-
