3eaa1b784815da6bf1bb9cb22554a67dba7432fa
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-06-18 08:54:44 -0500

    chore: :construction: Endpoints actualizados
    Se hace la actualización de los endpoints para su correcto funcionamiento

--- js/components/env.js
+++ js/components/env.js
@@ -1,4 +1,4 @@
 export const headers = {
-    'x-rapidapi-key': 'a7b32ee47amsh43afbe028ef9858p1a0e10jsna428de192cfe',
+    'x-rapidapi-key': '659e283b64msh40dd577d7f53426p105710jsnb4fb35aef390',
 	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 }
