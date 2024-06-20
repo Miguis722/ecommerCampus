88dd19f2c4475ffade58070aebd6339aabe7642d
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-06-20 08:48:53 -0500

    feat: :construction: Wishlist
    Se hace el formato del wishlist para que sea agradable y bonito a la vista, además de que se empieza a dar su respectivo espacio en los JS, además que se realizo un cambio en las keys de la API debido a que se acabaron

--- js/components/env.js
+++ js/components/env.js
@@ -1,4 +1,4 @@
 export const headers = {
-    'x-rapidapi-key': 'f0ca65cd7amshefeab28c25f663bp1820bbjsn343ca0734d5e',
+    'x-rapidapi-key': '4e3623f078mshfe3689b98bdeff4p177613jsnb4c0afda1407',
 	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 }
